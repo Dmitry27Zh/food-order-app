7ea0620496023d930b052e59979df90b60ec4dd0
Dmitry <dmitry27zh@gmail.com>
2023-09-16 13:53:55 +0300

    Start Meals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from './components/Layout/header/header'
+import Meals from './components/Meals/meals'
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   )
 }
---
+++ src/components/Meals/meals.jsx
@@ -0,0 +1,13 @@
+import MealList from './mealList/mealList'
+import PromoText from './promoText/promoText'
+
+const Meals = () => {
+  return (
+    <>
+      <PromoText />
+      <MealList />
+    </>
+  )
+}
+
+export default Meals

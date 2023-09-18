d5639355d3c6a2f881b22a34f399a9a7fd7469e8
Dmitry <dmitry27zh@gmail.com>
2023-09-18 11:59:39 +0300

    Add animation in HeaderCartButton

--- src/components/Layout/headerCartButton/headerCartButton.jsx
+++ src/components/Layout/headerCartButton/headerCartButton.jsx
@@ -1,15 +1,38 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import CartIcon from '../../Cart/cartIcon'
 import styles from './headerCartButton.module.css'
 import CartContext from '../../../store/cart-context'
 
 const HeaderCartButton = (props) => {
+  const [isAnimated, setIsAnimated] = useState(false)
   const { onClick } = props
   const cartContext = useContext(CartContext)
   const count = cartContext.items.reduce((result, item) => result + item.amount, 0)
+  const getClasses = () => {
+    let classes = `${styles.button}`
+
+    if (isAnimated) {
+      classes += ` ${styles.bump}`
+    }
+
+    return classes
+  }
+  useEffect(() => {
+    if (cartContext.items.length > 0) {
+      setIsAnimated(true)
+    }
+
+    const timerId = setTimeout(() => {
+      setIsAnimated(false)
+    }, 300)
+
+    return () => {
+      clearTimeout(timerId)
+    }
+  }, [cartContext.items])
 
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={getClasses()} onClick={onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
